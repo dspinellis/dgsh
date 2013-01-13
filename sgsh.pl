288f63232c5e0524873fff2f5c2b21f3652605ab
Diomidis Spinellis <dds@aueb.gr>
2013-01-13 22:00:11 +0200

    Add license comments

--- sgsh.pl
+++ sgsh.pl
@@ -4,6 +4,20 @@
 # Produce as output and execute a plain shell script implementing the
 # specified operations through named pipes
 #
+#  Copyright 2012-2013 Diomidis Spinellis
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 
 use strict;
 use warnings;
