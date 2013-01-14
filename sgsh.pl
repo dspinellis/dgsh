930b125a861186fb18371a0988c378a27c483aaa
Diomidis Spinellis <dds@aueb.gr>
2013-01-14 18:23:24 +0200

    Define operator regular expressions as variables
    This allows us to define the expressions in a single point (DRY),
    and improve their preciseness constistently across the program.

--- sgsh.pl
+++ sgsh.pl
@@ -74,6 +74,24 @@ my @lines;
 # User-specified input file name (or STDIN)
 my $input_filename;
 
+# Regular expressions for the input files scatter-gather operators
+# scatter |{
+my $SCATTER_BLOCK_BEGIN = q!^[^'#"]*scatter\s*\|\{\s*(\#.*)?$!;
+# |{
+my $SCATTER_BEGIN = q!\|\{\s*(\#.*)?$!;
+# |} gather |{
+my $GATHER_BLOCK_BEGIN = q!^[^'#"]*\|\}\s*gather\s*\|\{\s*(\#.*)?$!;
+# |}
+my $BLOCK_END = q!^[^'#"]*\|\}\s*(\#.*)?$!;
+# -|
+my $SCATTER_INPUT = q!^[^'#"]*-\|!;
+# |= name
+my $GATHER_VARIABLE_OUTPUT = q!\|\=\s*(\w+)\s*(\#.*)?$!;
+# |>/sgsh/name
+my $GATHER_FILE_OUTPUT = q!\|\>\s*\/sgsh\/(\w+)\s*(\#.*)?$!;
+# -||>/sgsh/name
+my $SCATTER_GATHER_PASS_THROUGH = q!^[^'#"]*-\|\|\>\s*\/sgsh\/(\w+)\s*(\#.*)?$!;
+
 # Read input file
 if ($#ARGV >= 0) {
 	$input_filename = shift;
@@ -97,7 +115,7 @@ $lines[0] =~ s/^\#\!/#/;
 for (my $i = 0; $i <= $#lines; $i++) {
 	$_ = $lines[$i];
 	# Scatter block begin
-	if (/^[^'#"]*scatter\s*\|\{\s*(\#.*)?$/) {
+	if (/$SCATTER_BLOCK_BEGIN/o) {
 		if ($in_scatter_gather_block) {
 			print STDERR "$input_filename(", $i + 1, "): Scatter-gather blocks can't be nested\n";
 			exit 1;
@@ -113,7 +131,7 @@ for (my $i = 0; $i <= $#lines; $i++) {
 		next;
 
 	# Gather block begin
-	} elsif (/^[^'#"]*\|\}\s*gather\s*\|\{\s*(\#.*)?$/) {
+	} elsif (/$GATHER_BLOCK_BEGIN/o) {
 		if ($#current_point_stack != -1) {
 			print STDERR "$input_filename(", $i + 1, "): Missing |}\n";
 			exit 1;
@@ -124,7 +142,7 @@ for (my $i = 0; $i <= $#lines; $i++) {
 		next;
 
 	# Scatter group end
-	} elsif (/^[^'#"]*\|\}\s*(\#.*)?$/) {
+	} elsif (/$BLOCK_END/o) {
 		if ($#current_point_stack == -1) {
 			print STDERR "$input_filename(", $i + 1, "): Extra |}\n";
 			exit 1;
@@ -134,23 +152,23 @@ for (my $i = 0; $i <= $#lines; $i++) {
 	}
 
 	# Scatter input endpoint
-	if (/^[^'#"]*-\|/) {
+	if (/$SCATTER_INPUT/o) {
 		$endpoint_number[$current_point]++;
 	}
 
 	# Scatter group begin
-	if (/\|\{\s*(\#.*)?$/) {
+	if (/$SCATTER_BEGIN/o) {
 		push(@current_point_stack, $current_point);
 		$current_point = ++$point_counter;
 	}
 
 	# Gather variable output endpoint
-	if (/\|\=\s*(\w+)\s*(\#.*)?$/) {
+	if (/$GATHER_VARIABLE_OUTPUT/o) {
 		$gather_variable_name[$gather_variable_points++] = $1;
 	}
 
 	# Gather file output endpoint
-	if (/\|\>\s*\/sgsh\/(\w+)\s*(\#.*)?$/) {
+	if (/$GATHER_FILE_OUTPUT/o) {
 		$gather_file_name[$gather_file_points++] = $1;
 	}
 
@@ -200,7 +218,7 @@ generate_scatter_code
 	for (my $i = $start; $i <= $end; $i++) {
 		$_ = $lines[$i];
 		# Scatter block begin: initialize named pipes
-		if (/scatter\s*\|\{/) {
+		if (/$SCATTER_BLOCK_BEGIN/o) {
 			# Generate initialization code
 			# The traps ensure that the named pipe directory
 			# is removed on termination and that the exit code
@@ -224,12 +242,12 @@ generate_scatter_code
 			s/scatter\s*\|\{/$code/;
 
 		# Gather group begin
-		} elsif (/\|\}\s*gather\s*\|\{/) {
+		} elsif (/$GATHER_BLOCK_BEGIN/o) {
 			generate_scatter_code($scatter_gather_start, $i - 1);
 			$i += generate_gather_code($i);
 
 		# Scatter group end: maintain stack
-		} elsif (/\|\}/) {
+		} elsif (/$BLOCK_END/o) {
 			$current_point = pop(@current_point_stack);
 			s/\|\}//;
 
@@ -237,19 +255,19 @@ generate_scatter_code
 
 		# Scatter-gather pass-through to a named file
 		# Pass the output through tuboflo to avoid deadlock
-		if (/-\|\|\>\s*\/sgsh\/(\w+)/) {
+		if (/$SCATTER_GATHER_PASS_THROUGH/o) {
 			s/-\|/<\$SGDIR\/npi-$current_point.$endpoint_counter[$current_point]/;
 			s/\|\>\s*\/sgsh\/(\w+)/ tuboflo >\$SGDIR\/npfo-$1 &/;
 		}
 
 		# Scatter input head endpoint: get input from named pipe
-		if (/-\|/) {
+		if (/$SCATTER_INPUT/o) {
 			s/-\|/<\$SGDIR\/npi-$current_point.$endpoint_counter[$current_point]/;
 			$endpoint_counter[$current_point]++;
 		}
 
 		# Scatter group begin: tee output to named pipes
-		if (/\|\{/) {
+		if (/$SCATTER_BEGIN/o) {
 			push(@current_point_stack, $current_point) if (defined($current_point));
 			$current_point = ++$point_counter;
 			my $tee_args;
@@ -263,13 +281,13 @@ generate_scatter_code
 		}
 
 		# Gather output endpoint to named variable
-		if (/\|\=\s*(\w+)/) {
+		if (/$GATHER_VARIABLE_OUTPUT/o) {
 			s/\|\=\s*(\w+)/>\$SGDIR\/npvo-$1 &/;
 		}
 
 		# Gather output endpoint to named file
 		# Pass the output through tuboflo to avoid deadlock
-		if (/\|\>\s*\/sgsh\/(\w+)/) {
+		if (/$GATHER_FILE_OUTPUT/) {
 			s/\|\>\s*\/sgsh\/(\w+)/| tuboflo >\$SGDIR\/npfo-$1 &/;
 		}
 
@@ -287,14 +305,14 @@ generate_gather_code
 	my $i;
 	for ($i = $start; $i <= $#lines; $i++) {
 			$_ = $lines[$i];
-		if (/\|\}\s*gather\s*\|\{/) {
+		if (/$GATHER_BLOCK_BEGIN/o) {
 			s/\|\}\s*gather\s*\|\{//;
 			print $output_fh "# Gather the results\n(\n";
 			for (my $j = 0; $j <= $#gather_variable_name; $j++) {
 				print $output_fh qq{\techo "$gather_variable_name[$j]='`cat \$SGDIR/npvo-$gather_variable_name[$j]`'" &\n};
 			}
 			print $output_fh "\twait\ncat <<\\SGEOFSG\n";
-		} elsif (/\|\}/) {
+		} elsif (/$BLOCK_END/o) {
 			s/\|\}//;
 
 			print $output_fh "SGEOFSG\n) | $opt_s\n";
