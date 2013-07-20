053ff05e5886de19afbb585acb3a36a87107cb90
Diomidis Spinellis <dds@aueb.gr>
2013-07-20 12:30:27 +0300

    Avoid deadlock caused by successive cat commands
    Each of the cat commands depends on its preceding one. Therefore,
    the corresponding stream file is not opened, until the preceding
    one finishes. This prevents the shell from opening the output
    redirection to the corresponding write end of the named pipe,
    and thereby never launching the corresponding command.
    
    Solve the problem by using a single cat command, which the
    d50f56d34 version of sgsh replaces with a call to sgsh-tee,
    which opens all the input files upon startup.
    
    TODO: Have sgsh automatically flag such deadlocks.

--- example/web-log-report.sh
+++ example/web-log-report.sh
@@ -3,7 +3,9 @@
 # SYNOPSIS Web log reporting
 # DESCRIPTION
 # Creates a report for a fixed-size web log file read from the standard input.
-# Demonstrates the processing reuse and the combined use of stores and streams.
+# Demonstrates the combined use of stores and streams, the use of shell group
+# commands and functions in the scatter block, and the use of cat(1) as
+# a way to sequentially combine multiple streams.
 # Used to measure throughput increase achieved through parallelism.
 #
 #  Copyright 2013 Diomidis Spinellis
@@ -66,12 +68,19 @@ scatter |{
 			|}
 
 			# Top 10 hosts
-			-| toplist 10 |>/stream/top10HostsByN
+			-| {
+				header 'Top 10 Hosts'
+				toplist 10
+			} |>/stream/top10HostsByN
 		|}
 
 		# Top 20 TLDs
-		-| awk -F. '$NF !~ /^[0-9]/ {print $NF}' |
-			sort | toplist 20 |>/stream/top20TLD
+		-| {
+			header 'Top 20 Level Domain Accesses'
+			awk -F. '$NF !~ /^[0-9]/ {print $NF}' |
+			sort |
+			toplist 20
+		} |>/stream/top20TLD
 
 		# Domains
 		-| awk -F. 'BEGIN {OFS = "."}
@@ -80,27 +89,40 @@ scatter |{
 			-| uniq | wc -l |store:nDomain
 
 			# Top 10 domains
-			-| toplist 10 |>/stream/top10Domain
+			-| {
+				header 'Top 10 Domains'
+				toplist 10
+			} |>/stream/top10Domain
 		|}
 	|}
 
 	# Hosts by volume
-	-| awk '    {bytes[$1] += $NF}
+	-| {
+		header 'Top 10 Hosts by Transfer'
+		awk '    {bytes[$1] += $NF}
 		END {for (h in bytes) print bytes[h], h}' |
 		sort -rn |
-		head -10 |>/stream/top10HostsByVol
+		head -10
+	} |>/stream/top10HostsByVol
 
 	# Sorted page name requests
 	-| awk '{print $7}' | sort |{
 
 		# Top 20 area requests (input is already sorted)
-		-| awk -F/ '{print $2}' | toplist 20 |>/stream/top20Area
+		-| {
+			header 'Top 20 Area Requests'
+			awk -F/ '{print $2}' |
+			toplist 20
+		} |>/stream/top20Area
 
 		# Number of different pages
 		-| uniq | wc -l |store:nPages
 
 		# Top 20 requests
-		-| toplist 20 |>/stream/top20Request
+		-| {
+			header 'Top 20 Requests'
+			toplist 20
+		} |>/stream/top20Request
 	|}
 
 	# Access time: dd/mmm/yyyy:hh:mm:ss
@@ -112,25 +134,33 @@ scatter |{
 			# Number of days
 			-| uniq | wc -l |store:nDays
 
-			-| uniq -c |>/stream/accessByDate
+			-| {
+				header 'Accesses by Date'
+				uniq -c
+			} |>/stream/accessByDate
 
 			# Accesses by day of week
-			-| sed 's|/|-|g' |
+			-| {
+				header 'Accesses by Day of Week'
+				sed 's|/|-|g' |
 				date -f - +%a |
 				sort |
 				uniq -c |
-				sort -rn |>/stream/accessByDoW
+				sort -rn
+			} |>/stream/accessByDoW
 		|}
 
 		# Hour
-		-| awk -F: '{print $2}' |
+		-| {
+			header 'Accesses by Local Hour'
+			awk -F: '{print $2}' |
 			sort |
-			uniq -c |>/stream/accessByHour
+			uniq -c
+		} |>/stream/accessByHour
 	|}
 
-|} gather |{
-
-cat <<EOF
+	.|
+		cat <<EOF
 			WWW server statistics
 			=====================
 
@@ -147,31 +177,10 @@ MBytes per day: $(expr $(store:nXBytes) / $(store:nDays) / 1024 / 1024)
 MBytes log file size: $(expr $(store:nLogBytes) / 1024 / 1024)
 
 EOF
+	|>/stream/summary
 
-header 'Top 20 Requests'
-cat /stream/top20Request
+|} gather |{
 
-header 'Top 20 Area Requests'
-cat /stream/top20Area
-
-header 'Top 10 Hosts'
-cat /stream/top10HostsByN
-
-header 'Top 10 Hosts by Transfer'
-cat /stream/top10HostsByVol
-
-header 'Top 10 Domains'
-cat /stream/top10Domain
-
-header 'Top 20 Level Domain Accesses'
-cat /stream/top20TLD
-
-header 'Accesses by Day of Week'
-cat /stream/accessByDoW
-
-header 'Accesses by Local Hour'
-cat /stream/accessByHour
-
-header 'Accesses by Date'
-cat /stream/accessByDate
+	# Gather all results together sequentially into a single report
+	cat /stream/summary /stream/top20Request /stream/top20Area /stream/top10HostsByN /stream/top10HostsByVol /stream/top10Domain /stream/top20TLD /stream/accessByDoW /stream/accessByHour /stream/accessByDate
 |}
