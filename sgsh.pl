5120756c8fad63e523a669aa87ef546722cbd838
Diomidis Spinellis <dds@aueb.gr>
2013-08-06 12:01:43 +0300

    Mark waited-on pipes as read

--- sgsh.pl
+++ sgsh.pl
@@ -307,6 +307,7 @@ while (get_next_line()) {
 			$no_output_wait = "${opt_p}sgsh-tee " .
 				join(' ', map(" -i $_", @gather_collect)) .
 				" >/dev/null\n";
+			grep($read_pipe{$_} = 1, @gather_collect);
 		}
 
 		# Prompt to exit debugging if debugging from an interactive session
