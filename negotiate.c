61bf5b5d8c0af6ece0f8cc6265ecd096ed0bee84
Marios Fragkoulis <mfg@aueb.gr>
2015-02-05 09:52:19 +0200

    Operations for getting SGSH_IN, SGSH_OUT environment variables.

--- negotiate.c
+++ negotiate.c
@@ -1,4 +1,30 @@
-#include "sgsh-negotiate.h"
+#include <stdlib.h> /* getenv() */
+#include <err.h> /* err() */
+#include "sgsh-negotiate.h" /* sgsh_negotiate(), sgsh_run() */
+
+
+#define OP_ERROR 1
+#define OP_SUCCESS 0
+
+int get_env_var(const char *env_var,int *value) {
+	char *string_value = getenv(env_var);
+	if (!string_value) {
+		err(1, "Getting environment variable \
+		%s failed.\n", env_var);
+		return OP_ERROR;
+	}
+	*value = atoi(string_value);
+	return OP_SUCCESS;
+}
+
+int get_environment_vars(int *sgsh_in, int *sgsh_out) {
+	int result;
+	result = get_env_var("SGSH_IN", sgsh_in);
+	if (!result) return OP_ERROR;
+	result = get_env_var("SGSH_OUT", sgsh_out);
+	if (!result) return OP_ERROR;
+	return OP_SUCCESS;
+}
 
 /* Each tool in the sgsh graph calls sgsh_negotiate() to take part in
  * peer-to-peer negotiation. A message block is circulated among tools
@@ -21,14 +47,17 @@ int sgsh_negotiate(const char *tool_name, /* Input. */
                     int n_input_fds, /* Number of input file descriptors. */
                     int *output_fds, /* Output file descriptors. */
                     int n_output_fds) { /* Number of output file descriptors. */
-
+	int sgsh_in, sgsh_out;
+	if (get_environment_vars(&sgsh_in, &sgsh_out) == OP_ERROR)
+		err(1, "Failed to extract SGSH_IN, SGSH_OUT \ 
+			environment variables.";)
 }
 
 /* If negotiation is successful, tools configure input and output 
  * according to the provided file descriptors and then they call
  * sgsh_run() to signal that they have set their input/output and
- * are ready for execution (or that they failed) by setting STATE_RUN. 
- * An algorithm
+ * are ready for execution (or that they failed) by setting STATE_RUN
+ * or STATE_ERROR. An algorithm
  * verifies that all tools completed this stage too successfully 
  * and the function returns success or failure.
  */
