9c594853e66db55cf7e81d6d64f29280342268c2
Marios Fragkoulis <mfg@aueb.gr>
2015-03-16 19:01:32 +0200

    Introducing unit testing through the Check framework. Work in progress.

---
+++ test/negotiation/check_negotiation.c
@@ -0,0 +1,34 @@
+#include <check.h> /* Check unit test framework API. */
+#include "../../sgsh_negotiate.h"
+
+START_TEST(test_negotiate_api)
+{
+	int *input_fds;
+	int n_input_fds;
+	int *output_fds;
+	int n_output_fds;
+	ck_assert_int_eq(sgsh_negotiate("test", 0, 0, &input_fds, &n_input_fds, 
+				&output_fds, &n_output_fds), PROT_STATE_ERROR);
+}
+END_TEST
+
+Suite *
+suite_negotiate(void)
+{
+	Suite *s = suite_create("Negotiate");
+	TCase *tc_core = tcase_create("Core");
+	tcase_add_test(tc_core, test_negotiate_api);
+	suite_add_tcase(s, tc_core);
+	return s;
+}
+
+int
+main(void) {
+	int number_failed;
+	Suite *s = suite_negotiate();
+	SRunner *sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
