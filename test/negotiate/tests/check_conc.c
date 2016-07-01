ab69d9b93ceb28b8565a73e3091a4456df8b3e5d
Marios Fragkoulis <mfg@aueb.gr>
2016-07-01 20:07:02 +0300

    Update unit test configuration for concentrator code (sgsh-conc).

--- test/negotiate/tests/check_conc.c
+++ test/negotiate/tests/check_conc.c
@@ -3,17 +3,13 @@
 #include <check.h>  /* Check unit test framework API. */
 #include <stdlib.h> /* EXIT_SUCCESS, EXIT_FAILURE */
 #include <stdbool.h> /* EXIT_SUCCESS, EXIT_FAILURE */
+#include "../src/sgsh-internal-api.h"
 
 START_TEST (test_name)
 {
-	/*extern bool multiple_inputs;
-	extern int nfd;
-	extern int next_fd(int fd);
-
 	multiple_inputs = true;
 	nfd = 5;
 	ck_assert_int_eq(next_fd(0), 1);
-*/
 }
 END_TEST
 
