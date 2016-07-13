f148753c1f3671b8dc2d8a5b0744a82ca6534482
Diomidis Spinellis <dds@aueb.gr>
2016-07-13 21:39:11 +0300

    Simplify negotiation invocation
    All the removed parts should be implemented in the negotiation API.

--- unix-sgsh-tools/simple_echo.c
+++ unix-sgsh-tools/simple_echo.c
@@ -1,56 +1,29 @@
-#include <stdio.h>		/* fdopen(), fprintf(), fputc() */
+#include <stdio.h>		/* printf(), putchar() */
 #include <assert.h>		/* assert */
-#include <sys/stat.h>		/* struct stat */
-#include <error.h>		/* error() */
-#include <errno.h>		/* errno */
 #include <unistd.h>		/* isatty() */
 #include <stdlib.h>		/* exit() */
+
 #include "sgsh-negotiate.h"
 
 int
 main(int argc, char *argv[])
 {
-	int ninputfds = -1, ninputfds_expected = 1;
-	int noutputfds = -1, noutputfds_expected = 0;
-	int *inputfds;
-	int *outputfds;
-	int status = -1;
-	struct stat stats;
-	int re = fstat(fileno(stdout), &stats);
-	FILE *ostream;
-	if (re < 0)
-		error(1, errno, "fstat failed\n");
+	int ninputfds, noutputfds;
+	int *inputfds, *outputfds;
 
-	if (isatty(fileno(stdin)))
-		ninputfds_expected = 0;
-
-	/* If standard output not connected to terminal and
-	 * connected to either a socket or a FIFO pipe
-	 * then its output channel is part of the sgsh graph
-	 */
-	if (!isatty(fileno(stdout)) &&
-			(S_ISFIFO(stats.st_mode) || S_ISSOCK(stats.st_mode)))
-		noutputfds_expected = 1;
-
-	if ((status = sgsh_negotiate("secho", ninputfds_expected, noutputfds_expected, &inputfds,
-					&ninputfds, &outputfds, &noutputfds)))
-	{
-		printf("sgsh negotiation failed with status code %d.\n", status);
+	if (sgsh_negotiate("secho", 0, 1, &inputfds, &ninputfds,
+			&outputfds, &noutputfds) != 0)
 		exit(1);
-	}
-	assert(noutputfds == noutputfds_expected);
 
-	if (noutputfds > 0)
-		ostream = fdopen(outputfds[0], "w");
-	else
-		ostream = stdout;
+	assert(ninputfds == 0);
+	assert(noutputfds == 1);
 
-	++argv;		/* skip program name */
+	++argv;
 	while (*argv) {
-		(void)fprintf(ostream, "%s", *argv);
+		(void)printf("%s", *argv);
 		if (*++argv)
-			fputc(' ', ostream);
-	}
-	fputc('\n', ostream);
+			putchar(' ');
+	putchar('\n');
+
 	return 0;
 }
