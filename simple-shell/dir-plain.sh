1e0b6439f2e8781bc7eac9194b576b5a68fac551
Marios Fragkoulis <mfg@aueb.gr>
2016-07-31 17:10:41 +0300

    Add test where bash sgsh script is provided from file

---
+++ simple-shell/dir-plain.sh
@@ -0,0 +1,9 @@
+SGSH=..
+
+$SGSH/sgsh-wrap ls -n \
+| $SGSH/sgsh-tee \
+| {{ \
+	$SGSH/sgsh-wrap awk '!/^total/ {print $6, $7, $8, $1, sprintf("%8d", $5), $9}' & \
+	$SGSH/sgsh-wrap awk '{s += $5} END {printf("%d bytes", s)}' & \
+}}
+	
