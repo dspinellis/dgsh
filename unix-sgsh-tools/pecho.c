76f0c972c06f52fe270772dcff539c9e5201e93c
Marios Fragkoulis <mfg@aueb.gr>
2016-10-17 12:49:54 +0300

    Add pecho.c

---
+++ unix-sgsh-tools/pecho.c
@@ -0,0 +1,33 @@
+#include <stdio.h>		/* printf() */
+#include <stdlib.h>		/* exit() */
+#include <unistd.h>		/* getpagesize() */
+#include "sgsh-negotiate.h"
+
+int
+main(int argc, char *argv[])
+{
+	int ps = getpagesize();
+	char buf[ps];
+	int n = 1;
+	if (sgsh_negotiate("secho", NULL, NULL, NULL, NULL) != 0)
+		exit(1);
+
+	++argv;
+	while (*argv) {
+		(void)printf("%s", *argv);
+		if (*++argv)
+			putchar(' ');
+	}
+
+	n = read(STDIN_FILENO, buf, ps);
+	while (n) {
+		printf("%s", buf);
+		n = read(STDIN_FILENO, buf, ps);
+		if (n < 0)
+			exit(1);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
