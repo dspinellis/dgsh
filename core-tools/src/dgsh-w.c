f5366d43789424ceec547de69b55f2e9e2fe330a
Marios Fragkoulis <mfg@aueb.gr>
2017-02-21 12:18:12 +0200

    Refactor dgsh-w.c (issue #67)

--- core-tools/src/dgsh-w.c
+++ core-tools/src/dgsh-w.c
@@ -8,9 +8,52 @@
 #include <string.h>	// memcpy()
 
 #include "dgsh.h"
-#include "debug.h"
+#include "dgsh-debug.h"
 
-int main(int argc, char** argv)
+void
+read_number(int fd, long double *x, long double complex *xc)
+{
+	char buf[sizeof(long double complex) + 5];	// \n\0
+	char real[sizeof(long double)];
+	char imag[sizeof(long double)];
+	int rd_size;
+
+	// Read input: 2 float values
+	rd_size = read(fd, buf, sizeof(buf));
+	if (rd_size == -1)
+		err(1, "write failed");
+	DPRINTF(4, "Read %zu characters, long double size: %zu, long double complex size: %zu",
+			rd_size, sizeof(long double), sizeof(long double complex));
+	if (rd_size == sizeof(long double)) {
+		memcpy(x, buf, sizeof(*x));
+		DPRINTF(4, "Read input x: %.10Lf", *x);
+	} else {
+		sscanf(buf, "%s %s", real, imag);
+		*xc = atof(real) + atof(imag)*I;
+		DPRINTF(4, "##xc: %.10f + %.10fi (read %zu characters)\n",
+				creal(*xc), cimag(*xc), rd_size);
+	}
+}
+
+void
+write_number(int fd, long double complex y)
+{
+	char buf[sizeof(long double complex)];
+	int wr_size;
+
+	// Write output: 2 float values
+	memset(buf, 0, sizeof(buf));
+	snprintf(buf, sizeof(buf), "%.10f %.10fi", creal(y), cimag(y));
+	DPRINTF(4, "##buf(y): %s, len: %d", buf, strlen(buf));
+	wr_size = write(fd, buf, sizeof(buf));
+	if (wr_size == -1)
+		err(1, "write failed");
+	DPRINTF(4, "##y: %.10f + %.10fi (wrote %zu characters)\n",
+			creal(y), cimag(y), wr_size);
+}
+
+int
+main(int argc, char** argv)
 {
 	int noutputfds = 2;
 	int *outputfds = NULL;
@@ -20,65 +63,31 @@ int main(int argc, char** argv)
 	long double complex xc1, xc2;
 	long double complex y1, y2, w, wmn;
 	size_t wr_size;
-	char buf[sizeof(long double complex) + 5];	// \n\0
-	char real[sizeof(long double)];
-	char imag[sizeof(long double)];
 	char negotiation_title[10];
 	int s;	// stage (stage=1,2,3)
 	int m;	// 2^stage
 	int n;	// nth root of unity
-	memset(buf, 0, sizeof(buf));
 
 	assert(argc == 3);
 	s = atoi(argv[1]);
 	m = pow(2, s);
 	n = atoi(argv[2]);
 
-	sprintf(negotiation_title, "%s %s %s", argv[0], argv[1], argv[2]);
+	snprintf(negotiation_title, sizeof(negotiation_title),
+			"%s %s %s", argv[0], argv[1], argv[2]);
 	dgsh_negotiate(DGSH_HANDLE_ERROR, negotiation_title, &ninputfds,
 			&noutputfds, &inputfds, &outputfds);
 	assert(ninputfds == 2);
 	assert(noutputfds == 2);
 
-	// Read input: 2 float values
-	wr_size = read(inputfds[0], buf, sizeof(buf));
-	if (wr_size == -1)
-		err(1, "write failed");
-	DPRINTF(4, "Read %zu characters, long double size: %zu, long double complex size: %zu",
-			wr_size, sizeof(long double), sizeof(long double complex));
-	if (wr_size == sizeof(long double)) {
-		memcpy(&x1, buf, sizeof(x1));
-		DPRINTF(4, "Read input x1: %.10Lf", x1);
-	} else {
-		//DPRINTF(4, "##buf(xc1): %s", buf);
-		sscanf(buf, "%s %s\n", real, imag);
-		xc1 = atof(real) + atof(imag)*I;
-		DPRINTF(4, "##xc1: %.10f + %.10fi (read %zu characters)\n",
-				creal(xc1), cimag(xc1), wr_size);
-	}
-
-	memset(buf, 0, sizeof(buf));
-	wr_size = read(inputfds[1], buf, sizeof(buf));
-	if (wr_size == -1)
-		err(1, "write failed");
-	DPRINTF(4, "Read %zu characters, long double size: %zu, long double complex size: %zu",
-			wr_size, sizeof(long double), sizeof(long double complex));
-	if (wr_size == sizeof(long double)) {
-		memcpy(&x2, buf, sizeof(x2));
-		DPRINTF(4, "Read input x2: %.10Lf", x2);
-	} else {
-		//DPRINTF(4, "##buf(xc2): %s", buf);
-		sscanf(buf, "%s %s\n", real, imag);
-		xc2 = atof(real) + atof(imag)*I;
-		DPRINTF(4, "##xc2: %.10f + %.10fi (read %zu characters)\n",
-				creal(xc2), cimag(xc2), wr_size);
-	}
+	read_number(inputfds[0], &x1, &xc1);
+	read_number(inputfds[1], &x2, &xc2);
 
 	// Calculate
 	w = 2 * M_PI * I / m;
 	wmn = cpow(cexp(w), n);
 	DPRINTF(4, "w: %.10f + %.10fi", creal(w), cimag(w));
-	DPRINTF(4, "##m: %d, n: %d, wmn: %.10f + %.10fi\n",
+	DPRINTF(4, "m: %d, n: %d, wmn: %.10f + %.10fi\n",
 			m, n, creal(wmn), cimag(wmn));
 	if (x1 == -1.0 && x2 == -1.0) {
 		y1 = xc1 + wmn * xc2;
@@ -88,24 +97,8 @@ int main(int argc, char** argv)
 		y2 = x1 - wmn * x2;
 	}
 
-	// Write output: 2 float values
-	memset(buf, 0, sizeof(buf));
-	sprintf(buf, "%.10f %.10fi\n", creal(y1), cimag(y1));
-	//DPRINTF(4, "##buf(y1): %s", buf);
-	wr_size = write(outputfds[0], buf, sizeof(buf));
-	if (wr_size == -1)
-		err(1, "write failed");
-	DPRINTF(4, "##y1: %.10f + %.10fi (wrote %zu characters)\n",
-			creal(y1), cimag(y1), wr_size);
-
-	memset(buf, 0, sizeof(buf));
-	sprintf(buf, "%.10f %.10fi\n", creal(y2), cimag(y2));
-	//DPRINTF(4, "##buf(y2): %s", buf);
-	wr_size = write(outputfds[1], buf, sizeof(buf));
-	if (wr_size == -1)
-		err(1, "write failed");
-	DPRINTF(4, "##y2: %.10f + %.10fi (wrote %zu characters)\n",
-			creal(y2), cimag(y2), wr_size);
+	write_number(outputfds[0], y1);
+	write_number(outputfds[1], y2);
 
 	return 0;
 }
