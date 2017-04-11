c557e8a67e638b24da4dc0d937266dbc61aa2ba2
Diomidis Spinellis <dds@aueb.gr>
2017-04-11 09:30:19 +0300

    Remove legacy comments

--- example/ft2d.sh
+++ example/ft2d.sh
@@ -33,24 +33,17 @@ sfsmooth rect2=2 |
 tee |
 {{
 	sfgrey pclip=100 wanttitle=n
-	#dgsh-writeval -s pulse.vpl
 
 	sffft1 |
 	sffft3 axis=2 pad=1 |
 	sfreal |
 	tee |
 	{{
-		sfwindow f1=1 |
-		sfreverse which=3
-
+		sfwindow f1=1 | sfreverse which=3
 		cat
-		#dgsh-tee -I |
-		#dgsh-writeval -s ft2d
 	}} |
-	sfcat axis=1 "<|" |	# dgsh-readval
-	sfgrey pclip=100 wanttitle=n \
-		label1="1/time" label2="1/space"
-	#dgsh-writeval -s ft2d.vpl
+	sfcat axis=1 "<|" |
+	sfgrey pclip=100 wanttitle=n label1="1/time" label2="1/space"
 }} |
 call_with_stdin side_by_side_iso '<|' '<|' yscale=1.25 >Fig/ft2dofpulse.vpl
 
@@ -62,43 +55,31 @@ sfput label2=space |
 sflmostretch delay=0 v0=-1 |
 tee |
 {{
-	sfwindow f2=1 |
-	sfreverse which=2
-
+	sfwindow f2=1 | sfreverse which=2
 	cat
-	#dgsh-tee -I | dgsh-writeval -s air
 }} |
 sfcat axis=2 "<|" |
 tee |
 {{
 	sfgrey pclip=100 wanttitle=n
-	#| dgsh-writeval -s airtx.vpl
 
 	sffft1 |
 	sffft3 sign=1 |
 	tee |
 	{{
 		sfreal
-		#| dgsh-writeval -s airftr
-
 		sfimag
-		#| dgsh-writeval -s airfti
 	}} |
-	dgsh-wrap -e -I sfmath nostdin=y re="<|" im="<|" output="sqrt(re*re+im*im)" |
+	dgsh-wrap -e -I sfmath nostdin=y re="<|" im="<|" \
+	  output="sqrt(re*re+im*im)" |
 	tee |
 	{{
-		sfwindow f1=1 |
-		sfreverse which=3
-
+		sfwindow f1=1 | sfreverse which=3
 		cat
-		#dgsh-tee -I | dgsh-writeval -s airft1
 	}} |
 	sfcat axis=1 "<|" |
-	sfgrey pclip=100 wanttitle=n label1="1/time" \
-		label2="1/space"
-	#| dgsh-writeval -s airfk.vpl
+	sfgrey pclip=100 wanttitle=n label1="1/time" label2="1/space"
 }} |
 call_with_stdin side_by_side_iso '<|' '<|' yscale=1.25 >Fig/airwave.vpl
-#call 'side_by_side_iso airtx.vpl airfk.vpl \
 
 wait
