cbbc2aa107e00f6ededf8531d5cea9b3a7f0ae44
Marios Fragkoulis <mfg@aueb.gr>
2017-08-07 12:24:19 +0300

    Differentiate installation of dependencies per platform for Travis

---
+++ .travis/macosx-xcode8.3.install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+- wget http://ftp.gnu.org/gnu/gettext/gettext-0.19.5.tar.xz
+- tar Jxvf gettext-0.19.5.tar.xz
+- cd gettext-0.19.5 && ./configure && make && sudo make install
+- cd ..
+- git clone --depth=1 -b madagascar-devel-2016 https://github.com/ahay/src.git madagascar
+- cd madagascar && rm -rf trip
+- sudo ./configure --prefix=/usr/local && sudo make && sudo make install
+- cd ..
+- mkdir nmrpipe && cd nmrpipe
+- wget https://www.ibbr.umd.edu/nmrpipe/install.com
+- wget https://www.ibbr.umd.edu/nmrpipe/binval.com
+- wget https://www.ibbr.umd.edu/nmrpipe/NMRPipeX.tZ
+- wget https://www.ibbr.umd.edu/nmrpipe/s.tZ
+- wget https://www.ibbr.umd.edu/nmrpipe/dyn.tZ
+- wget https://www.ibbr.umd.edu/nmrpipe/talos.tZ
+- wget http://spin.niddk.nih.gov/bax/software/smile/plugin.smile.tZ
+- chmod a+rx *.com && ./install.com
+- sudo install nmrbin.linux212_64/var2pipe nmrbin.linux212_64/nmrPipe /usr/local/bin
+- sudo install nmrbin.linux212_64/addNMR /usr/bin
+- cd ..
+- make config && make
