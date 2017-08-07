0e51cfd0543beb68e1605d1880a07495b589e6a3
Marios Fragkoulis <mfg@aueb.gr>
2017-08-07 22:31:38 +0300

    Satisfy dependencies for MacOS Travis build:   texinfo package

--- .travis/macosx-xcode8.3.install.sh
+++ .travis/macosx-xcode8.3.install.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-brew install check xz
+brew install check xz texinfo
 wget http://ftp.gnu.org/gnu/gettext/gettext-0.19.5.tar.xz
 tar Jxvf gettext-0.19.5.tar.xz >/dev/null
 cd gettext-0.19.5 && ./configure && make && sudo make install >/dev/null
