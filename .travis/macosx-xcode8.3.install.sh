e1edd902bafc5f66fcbd8c41196ce44ab4c54bed
Marios Fragkoulis <mfg@aueb.gr>
2017-08-07 15:42:08 +0300

    Reduce Travis build script output for MacOS

--- .travis/macosx-xcode8.3.install.sh
+++ .travis/macosx-xcode8.3.install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget http://ftp.gnu.org/gnu/gettext/gettext-0.19.5.tar.xz
-tar Jxvf gettext-0.19.5.tar.xz
-cd gettext-0.19.5 && ./configure && make && sudo make install
+tar Jxvf gettext-0.19.5.tar.xz >/dev/null
+cd gettext-0.19.5 && ./configure && make && sudo make install >/dev/null
 cd ..
 #git clone --depth=1 -b madagascar-devel-2016 https://github.com/ahay/src.git madagascar
 #cd madagascar && rm -rf trip
@@ -16,8 +16,8 @@ wget https://www.ibbr.umd.edu/nmrpipe/s.tZ
 wget https://www.ibbr.umd.edu/nmrpipe/dyn.tZ
 wget https://www.ibbr.umd.edu/nmrpipe/talos.tZ
 wget http://spin.niddk.nih.gov/bax/software/smile/plugin.smile.tZ
-chmod a+rx *.com && ./install.com
-sudo install nmrbin.linux212_64/var2pipe nmrbin.linux212_64/nmrPipe /usr/local/bin
-sudo install nmrbin.linux212_64/addNMR /usr/bin
+chmod a+rx *.com && ./install.com >/dev/null
+sudo install nmrbin.linux212_64/var2pipe nmrbin.linux212_64/nmrPipe /usr/local/bin >/dev/null
+sudo install nmrbin.linux212_64/addNMR /usr/bin >/dev/null
 cd ..
 make config && make
