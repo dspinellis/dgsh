544df86e89096e659f5d28aeafae86b6a85eac2a
Marios Fragkoulis <mfg@aueb.gr>
2017-08-08 10:47:14 +0300

    Make Travis build job for MacOS lighter by removing optional and custom tool installations

--- .travis/macosx-xcode8.3.install.sh
+++ .travis/macosx-xcode8.3.install.sh
@@ -13,16 +13,16 @@ cd ..
 #cd madagascar && rm -rf trip
 #sudo ./configure --prefix=/usr/local && sudo make && sudo make install
 #cd ..
-mkdir nmrpipe && cd nmrpipe
-wget https://www.ibbr.umd.edu/nmrpipe/install.com
-wget https://www.ibbr.umd.edu/nmrpipe/binval.com
-wget https://www.ibbr.umd.edu/nmrpipe/NMRPipeX.tZ
-wget https://www.ibbr.umd.edu/nmrpipe/s.tZ
-wget https://www.ibbr.umd.edu/nmrpipe/dyn.tZ
-wget https://www.ibbr.umd.edu/nmrpipe/talos.tZ
-wget http://spin.niddk.nih.gov/bax/software/smile/plugin.smile.tZ
-chmod a+rx *.com && ./install.com >/dev/null
-sudo install nmrbin.linux212_64/var2pipe nmrbin.linux212_64/nmrPipe /usr/local/bin >/dev/null
-sudo install nmrbin.linux212_64/addNMR /usr/bin >/dev/null
-cd ..
+#mkdir nmrpipe && cd nmrpipe
+#wget https://www.ibbr.umd.edu/nmrpipe/install.com
+#wget https://www.ibbr.umd.edu/nmrpipe/binval.com
+#wget https://www.ibbr.umd.edu/nmrpipe/NMRPipeX.tZ
+#wget https://www.ibbr.umd.edu/nmrpipe/s.tZ
+#wget https://www.ibbr.umd.edu/nmrpipe/dyn.tZ
+#wget https://www.ibbr.umd.edu/nmrpipe/talos.tZ
+#wget http://spin.niddk.nih.gov/bax/software/smile/plugin.smile.tZ
+#chmod a+rx *.com && ./install.com >/dev/null
+#sudo install nmrbin.linux212_64/var2pipe nmrbin.linux212_64/nmrPipe /usr/local/bin >/dev/null
+#sudo install nmrbin.linux212_64/addNMR /usr/bin >/dev/null
+#cd ..
 make config && make
