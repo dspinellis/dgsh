fbb7bb9ee79560c5c42a2223e9510210c93f6ff4
Marios Fragkoulis <mfg@aueb.gr>
2017-08-08 11:40:11 +0300

    Clarify dependencies for Ubuntu Linux

--- .travis/linux-ubuntu-trusty.install.sh
+++ .travis/linux-ubuntu-trusty.install.sh
@@ -2,9 +2,9 @@
 
 sudo apt-get -qq update
   # For installation
-sudo apt-get install -y make automake gcc libtool pkg-config texinfo help2man
+sudo apt-get install -y make automake gcc libtool pkg-config texinfo help2man autopoint bison check gperf 
   # For testing
-sudo apt-get install -y check gperf wbritish wamerican libfftw3-dev csh
+sudo apt-get install -y wbritish wamerican libfftw3-dev csh
 wget http://ftp.gnu.org/gnu/gettext/gettext-0.19.5.tar.xz
 tar Jxvf gettext-0.19.5.tar.xz
 cd gettext-0.19.5 && ./configure && make && sudo make install
