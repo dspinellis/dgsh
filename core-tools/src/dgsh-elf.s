04afa6b64e28674d45c944bab1cc18a961b6c436
Marios Fragkoulis <mfg@aueb.gr>
2018-10-08 21:11:12 +0200

    Fix style (alignment) in Assembly file

--- core-tools/src/dgsh-elf.s
+++ core-tools/src/dgsh-elf.s
@@ -16,13 +16,13 @@
 
 #elif __APPLE__
 
-	.section ".note.ident", "a"
-	.asciz "DSpinellis/dgsh"
-	.section	__TEXT,__text,regular,pure_instructions
-	.macosx_version_min 10, 13
-	.globl	_dgsh_force_include     ## @dgsh_force_include
-.zerofill __DATA,__common,_dgsh_force_include,4,2
+    .section ".note.ident", "a"
+    .asciz "DSpinellis/dgsh"
+    .section	__TEXT,__text,regular,pure_instructions
+    .macosx_version_min 10, 13
+    .globl	_dgsh_force_include     ## @dgsh_force_include
+    .zerofill __DATA,__common,_dgsh_force_include,4,2
 
-.subsections_via_symbols
+    .subsections_via_symbols
 
 #endif
