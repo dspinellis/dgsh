9e695a59c3d6ce7de14fdb6d9f9613454cf7281b
Diomidis Spinellis <dds@aueb.gr>
2020-09-23 10:14:36 +0300

    Use portable comments
    Issue: #113

--- core-tools/src/dgsh-elf.s
+++ core-tools/src/dgsh-elf.s
@@ -1,15 +1,20 @@
-# ELF note header to mark dgsh-compatible programs
-# See http://www.netbsd.org/docs/kernel/elf-notes.html
+/*
+ * ELF note header to mark dgsh-compatible programs
+ * See http://www.netbsd.org/docs/kernel/elf-notes.html
+ * Don't use line comments as these are not portable between
+ * different CPU architectures.
+ * https://en.wikipedia.org/wiki/GNU_Assembler#Single-Line_comments
+ */
 
     .comm dgsh_force_include,4,4
     .section ".note.ident", "a"
     .p2align 2
-    .long 1f - 0f		# name size (not including padding)
-    .long 3f - 2f		# desc size (not including padding)
-    .long 1			# type
-0:  .asciz "DSpinellis/dgsh"	# name
+    .long 1f - 0f		/* name size (not including padding) */
+    .long 3f - 2f		/* desc size (not including padding) */
+    .long 1			/* type */
+0:  .asciz "DSpinellis/dgsh"	/* name */
 1:  .p2align 2
-2:  .long 0x00000001		# desc
+2:  .long 0x00000001		/* desc */
     .long 0x00000000
 3:  .p2align 2
 
